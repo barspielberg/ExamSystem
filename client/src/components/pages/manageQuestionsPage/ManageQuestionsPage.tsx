0479e619642f04e0d5f32c97568ef93364c45979
Bar Spielberg <barspielberg@gmail.com>
2021-01-14 17:00:26 +0200

    upadate routes

--- client/src/components/pages/manageQuestionsPage/ManageQuestionsPage.tsx
+++ client/src/components/pages/manageQuestionsPage/ManageQuestionsPage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { match } from "react-router";
 
-interface IManageQuestionsPageProps {}
+interface IManageQuestionsPageProps {
+  match: match<{ fieldId: string }>;
+}
 //TODO by Michael
-const ManageQuestionsPage: React.FC<IManageQuestionsPageProps> = ({}) => {
-  return <div>ManageQuestionsPage Worked!</div>;
+const ManageQuestionsPage: React.FC<IManageQuestionsPageProps> = ({
+  match,
+}) => {
+  return <div>ManageQuestionsPage Worked! id: {match.params.fieldId}</div>;
 };
 
 export default ManageQuestionsPage;
--- client/src/components/pages/studentReportPage/StudentReportPage.tsx
+++ client/src/components/pages/studentReportPage/StudentReportPage.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { match } from "react-router";
 
-interface IStudentReportPageProps {}
+interface IStudentReportPageProps {
+  match: match<{ fieldId: string }>;
+}
 //TODO by Michael
-const StudentReportPage: React.FC<IStudentReportPageProps> = ({}) => {
-  return <div>StudentReportPage Worked!</div>;
+const StudentReportPage: React.FC<IStudentReportPageProps> = ({ match }) => {
+  return <div>StudentReportPage Worked! id: {match.params.fieldId}</div>;
 };
 
 export default StudentReportPage;
