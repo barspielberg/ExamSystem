5efe357375ff5e8e0aaa8805af30392ad7ff0450
Michael <mishatolchinski@gmail.com>
2021-01-14 19:16:12 +0200

    login updated

--- server/src/Dal/organizationRepository.ts
+++ server/src/Dal/organizationRepository.ts
@@ -2,17 +2,18 @@ const fs = require("fs");
 const util = require("util");
 const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
-const adminDb = '../../data/admins.json';
 import { Admin } from '../../../common/index';
+const adminDb = require('../../data/admins.json');
 
 class OrganizationRepository {
-  async checkAdminExists(email: string, password: string) {
-    const data = JSON.parse(await readFile(adminDb));
-    const admin = data.find((adm: Admin) => {
-      return adm.email === email && adm.password === password;
-    });
+  async checkAdminExists(email: string | any, password: string | any) {
+    // const data = JSON.parse(await readFile(adminDb));
+    console.log(adminDb);
+    // const admin = data.find((adm: Admin) => {
+    //   return adm.email === email && adm.password === password;
+    // });
 
-    return admin;
+    // return admin;
   }
 
 
--- server/src/controllers/organizationController.ts
+++ server/src/controllers/organizationController.ts
@@ -2,12 +2,13 @@ import { NextFunction, Request, Response } from "express";
 import organozationRepository from '../Dal/organizationRepository';
 
 class QuestionsController {
-  getOrganization(req: Request, res: Response, next: NextFunction) {
+   getOrganization(req: Request, res: Response, next: NextFunction) {
     //TODO check credentials and return org or error
+    const {email,password} = req.query;
 
-    console.log(req.params);
     try {
-      //const admin = organozationRepository.checkAdminExists()
+      const admin = organozationRepository.checkAdminExists(email, password);
+     // console.log("nananan",admin);
     } catch (error) {
       
     }
