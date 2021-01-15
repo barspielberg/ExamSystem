a465118c1e0cbaa348de44d5eff1c46b6b6262c4
Michael <mishatolchinski@gmail.com>
2021-01-15 10:12:03 +0200

    updated login

--- server/src/Dal/organizationRepository.ts
+++ server/src/Dal/organizationRepository.ts
@@ -1,43 +1,8 @@
-<<<<<<< HEAD
-const fs = require("fs");
-const util = require("util");
-const writeFile = util.promisify(fs.writeFile);
-const readFile = util.promisify(fs.readFile);
-import { Admin } from '../../../common/index';
-import adminDb from '../data/admins.json';
-import organizationsDb from '../data/organizations.json';
-
-class OrganizationRepository {
-  checkAdminExists(email: any, password: any) {
-    const {admins } = adminDb;
-    
-    const admin = admins.find((adm: Admin) => {
-      return adm.email === email && adm.password === password;
-    });
-
-    if(admin) return admin;
-    else return null;
-  }
-
-
-  getOraganization(admin:Admin) {
-    const org = organizationsDb.organizations.find(org => org.adminIds.includes(admin.id));
-    if (org) return org;
-    else return null;
-  }
-
-=======
-// const fs = require("fs");
-// const util = require("util");
-// const writeFile = util.promisify(fs.writeFile);
-// const readFile = util.promisify(fs.readFile);
 import { Admin, Organization } from "../../../common/index";
-//TODO import admindb file, fix file system bug first.
 import adminDb from "../data/admins.json";
 import organizationDb from "../data/organizations.json";
 
 class OrganizationRepository {
-  //why string | any ?
   async checkAdminExists(email: string, password: string) {
     const admins: Admin[] = adminDb.admins;
     return admins.find(
@@ -49,7 +14,6 @@ class OrganizationRepository {
     const organizations: Organization[] = organizationDb.organizations;
     return organizations.find((o) => o.adminIds.includes(admin.id));
   }
->>>>>>> c29a44c1330945d014d63361e90a482f5ba32034
 }
 
 export default new OrganizationRepository();
--- server/src/controllers/organizationController.ts
+++ server/src/controllers/organizationController.ts
@@ -1,39 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import organizationRepository from "../Dal/organizationRepository";
 
-<<<<<<< HEAD
-class QuestionsController {
-  getOrganization(req: Request, res: Response, next: NextFunction) {
-    const { email, password } = req.query;
-
-    try {
-      const admin = organozationRepository.checkAdminExists(email, password);
-      if (admin) {
-        const org = organozationRepository.getOraganization(admin);
-        if (org) {
-          res.status(200).json({
-            message: 'Organization fetched successfully',
-            organization: org
-          });
-        } else {
-          res.status(500).json({
-            message: 'Fetching Organization failed'
-          });
-        }
-      } else {
-        res.status(500).json({
-          message: 'Wrong Credentials, try again or sign up'
-        });
-      }
-    } catch (error) {
-      res.status(500).json({
-        message: 'OOPS something went wrong'
-      });
-    }
-=======
 class OrganozationController {
   async getOrganization(req: Request, res: Response, next: NextFunction) {
-    //TODO check credentials and return org or error
     const { email, password } = req.query;
     const emailSt = email?.toString();
     const passwordSt = password?.toString();
@@ -53,8 +22,10 @@ class OrganozationController {
         return res.status(401).send("there are no organization for that admin");
 
       return res.status(200).json(organization);
-    } catch (error) {}
->>>>>>> c29a44c1330945d014d63361e90a482f5ba32034
+    } catch (error) {
+      res.status(500).json("OOPS something went wrong");
+    }
+
   }
 }
 
