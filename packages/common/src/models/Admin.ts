158b94edfba391500a0a4947ff0c6063cb1121d3
Bar Spielberg <barspielberg@gmail.com>
2021-01-17 19:46:01 +0200

    add fields in admin

--- packages/common/src/models/Admin.ts
+++ packages/common/src/models/Admin.ts
@@ -1,5 +1,9 @@
+import { Organization } from "./Organization";
+
 export interface Admin {
   id: string;
   email: string;
   password: string;
+  organizationIds: string[];
+  organizations: Organization[];
 }
