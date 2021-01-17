4356f9b4098cb0052552d177993a16a6bdcf9ba2
Bar Spielberg <barspielberg@gmail.com>
2021-01-17 12:45:11 +0200

    fix repository bags

--- packages/server/src/Dal/organizationRepository.ts
+++ packages/server/src/Dal/organizationRepository.ts
@@ -1,17 +1,31 @@
 import { Admin, Organization } from "@examsystem/common";
-import adminDb from "../data/admins.json";
-import organizationDb from "../data/organizations.json";
+import { promises as fsPromises } from "fs";
+import { resolve as pathResolve } from "path";
+// import "../data/admins.json";
+// import "../data/organizations.json";
+
+const adminPath = pathResolve(__dirname, "..", "..", "data", "admins.json");
+const organizationPath = pathResolve(
+  __dirname,
+  "..",
+  "..",
+  "data",
+  "organizations.json"
+);
 
 class OrganizationRepository {
   async checkAdminExists(email: string, password: string) {
-    const admins: Admin[] = adminDb.admins;
+    const adminStr = await fsPromises.readFile(adminPath, "utf8");
+
+    const admins: Admin[] = JSON.parse(adminStr).admins;
     return admins.find(
       (adm) => adm.email === email && adm.password === password
     );
   }
 
   async getOrganization(admin: Admin) {
-    const organizations: Organization[] = organizationDb.organizations;
+    const orgStr = await fsPromises.readFile(organizationPath, "utf8");
+    const organizations: Organization[] = JSON.parse(orgStr).organizations;
     return organizations.find((o) => o.adminIds.includes(admin.id));
   }
 }
