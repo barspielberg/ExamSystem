60af8a62ff8b98a5cc8cf156ea839eba8f3a4aa2
Bar Spielberg <barspielberg@gmail.com>
2021-01-21 13:55:53 +0200

    selection

--- packages/client/src/components/pages/editTestPage/FormSections/QuestionsSection/SelectQuestions/SelectQuestions.tsx
+++ packages/client/src/components/pages/editTestPage/FormSections/QuestionsSection/SelectQuestions/SelectQuestions.tsx
@@ -1,5 +1,7 @@
-import { Question, Test } from "@examsystem/common";
 import React from "react";
+import classes from "./SelectQuestions.module.scss";
+import { Alignment, Question, QuestionType, Test } from "@examsystem/common";
+import { Button } from "../../../../../uiElements";
 
 interface ISelectQuestionsProps {
   test: Test;
@@ -10,6 +12,7 @@ interface ISelectQuestionsProps {
 const SelectQuestions: React.FC<ISelectQuestionsProps> = ({
   test,
   onTestChange,
+  questions,
 }) => {
   const addQuestion = (id: string) => {
     onTestChange({ ...test, questionIds: [...test.questionIds, id] });
@@ -20,7 +23,64 @@ const SelectQuestions: React.FC<ISelectQuestionsProps> = ({
       questionIds: test.questionIds.filter((i) => i !== id),
     });
   };
-  return <div>SelectQuestions Worked!</div>;
+
+  const rowClicked = (id: string) => {
+    if (test.questionIds.includes(id)) removeQuestion(id);
+    else addQuestion(id);
+  };
+
+  return (
+    <div className={classes.questions}>
+      <header>
+        <div>Currently showing {questions?.length} questions</div>
+        <div>
+          Total selected for the test{" "}
+          <span className={classes.num}>{test.questionIds.length}</span>{" "}
+        </div>
+      </header>
+
+      {tmpQues.map((q) => (
+        <div
+          key={q.id}
+          className={classes.row}
+          aria-selected={test.questionIds.includes(q.id)}
+        >
+          <div className={classes.des} onClick={() => rowClicked(q.id)}>
+            <b>{q.mainTitle}</b>
+            <h5>{q.secondaryTitle}</h5>
+            <div className={classes.tags}>
+              {q.tags.map((t, index) => (
+                <span key={index}>{t}</span>
+              ))}
+            </div>
+          </div>
+          <div className={classes.showBtn}>
+            <Button>Show</Button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SelectQuestions;
+
+const tmpQue: Question = {
+  id: "13588551315",
+  mainTitle: "The Main Tilte",
+  secondaryTitle: "Optional Secondary Title",
+  alignment: Alignment.vertical,
+  type: QuestionType.singleChoiceQuestion,
+  tags: ["first", "second", "therd"],
+  possibleAnswers: [
+    { id: "4545923288", content: "Yes!", correct: true },
+    { id: "454923f288", content: "no" },
+    { id: "45459228f8", content: "no" },
+    { id: "45s4923288", content: "no" },
+  ],
+};
+
+const tmpQues: Question[] = ["41", "56", "12", "5689", "sdf"].map((id) => ({
+  ...tmpQue,
+  id: id,
+}));
