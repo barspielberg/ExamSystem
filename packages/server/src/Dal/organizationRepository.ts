39d80d327fd7cf29000b2901bce3eb370a31133e
Bar Spielberg <barspielberg@gmail.com>
2021-01-25 16:09:53 +0200

    add new test server

--- packages/server/src/Dal/organizationRepository.ts
+++ packages/server/src/Dal/organizationRepository.ts
@@ -25,8 +25,10 @@ class OrganizationRepository {
 
   async getQuestion(orgId: string, adminId: string, questionId: string) {
     const orgs = await this.getOrganizations(adminId);
-    const org = orgs.find(o => o.id === orgId && o.adminIds.includes(adminId));
-    const question = org?.questions.find(q => q.id === questionId);
+    const org = orgs.find(
+      (o) => o.id === orgId && o.adminIds.includes(adminId)
+    );
+    const question = org?.questions.find((q) => q.id === questionId);
     return question;
   }
 
@@ -35,12 +37,11 @@ class OrganizationRepository {
     const organizations = await this.getAllOrganizations();
     const foundOrg = organizations.find((o) => o.id === orgId);
     foundOrg?.questions.push(question);
-    foundOrg?.fields.map(field => {
-      if (fieldsIds.includes(field.id))
-        field.questionIds.push(question.id);
+    foundOrg?.fields.map((field) => {
+      if (fieldsIds.includes(field.id)) field.questionIds.push(question.id);
     });
 
-    const dbQuestion = foundOrg?.questions.find(q => q.id === question.id);
+    const dbQuestion = foundOrg?.questions.find((q) => q.id === question.id);
 
     const stringifiedOrgs = JSON.stringify({ organizations: organizations });
     await fsPromises.writeFile(organizationPath, stringifiedOrgs, "utf8");
@@ -54,12 +55,15 @@ class OrganizationRepository {
       if (q.id === question.id) foundOrg.questions[index] = question;
     });
 
-    foundOrg?.fields.map(field => {
-      if (fieldsIds.includes(field.id) && !field.questionIds.includes(question.id))
+    foundOrg?.fields.map((field) => {
+      if (
+        fieldsIds.includes(field.id) &&
+        !field.questionIds.includes(question.id)
+      )
         field.questionIds.push(question.id);
     });
 
-    const dbQuestion = foundOrg?.questions.find(q => q.id === question.id);
+    const dbQuestion = foundOrg?.questions.find((q) => q.id === question.id);
 
     const stringifiedOrgs = JSON.stringify({ organizations: organizations });
     await fsPromises.writeFile(organizationPath, stringifiedOrgs, "utf8");
@@ -77,6 +81,21 @@ class OrganizationRepository {
     if (index != undefined && index != -1) {
       tests?.splice(index, 1, test);
     }
+
+    const dbTest = tests?.find((t) => t.id === test.id);
+
+    const stringifiedOrgs = JSON.stringify({ organizations: organizations });
+    await fsPromises.writeFile(organizationPath, stringifiedOrgs, "utf8");
+    return dbTest;
+  }
+  async postTest(orgId: string, fieldId: string, test: Test) {
+    const organizations = await this.getAllOrganizations();
+
+    const tests = organizations
+      .find((o) => o.id === orgId)
+      ?.fields.find((f) => f.id === fieldId)?.tests;
+    test.id = getNewId();
+    tests?.push(test);
 
     const dbTest = tests?.find((t) => t.id === test.id);
 
--- packages/server/src/controllers/testsController.ts
+++ packages/server/src/controllers/testsController.ts
@@ -22,6 +22,30 @@ class TestsController {
         .json({ message: "OOPS somwthing went wrong", error: error });
     }
   }
+  async postTest(req: Request, res: Response, next: NextFunction) {
+    const { orgId, fieldId, test } = req.body;
+    if (!orgId || !fieldId || !test)
+      return res
+        .status(400)
+        .json({ message: "OOPS it seems like you are missing some inputs" });
+
+    try {
+      const dbTest = await organizationRepository.postTest(
+        orgId,
+        fieldId,
+        test
+      );
+      if (dbTest)
+        return res
+          .status(200)
+          .json({ message: "Test updated successfully", test: dbTest });
+      else res.status(410).json({ message: "Test not found" });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "OOPS somwthing went wrong", error: error });
+    }
+  }
 }
 
 export default new TestsController();
