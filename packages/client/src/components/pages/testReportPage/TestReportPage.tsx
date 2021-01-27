2020399b3a86002ecdc5d29cb71643a4994ccd4c
Michael <mishatolchinski@gmail.com>
2021-01-27 16:16:30 +0200

    updated report

--- packages/client/src/components/pages/testReportPage/TestReportPage.tsx
+++ packages/client/src/components/pages/testReportPage/TestReportPage.tsx
@@ -1,6 +1,6 @@
 import classes from "./TestReportPage.module.scss";
-import { Organization } from "@examsystem/common";
-import React from "react";
+import { Organization, Test } from "@examsystem/common";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { useParams } from "../../../hooks";
 import { RootState } from "../../../redux/reducers/mainReducer";
@@ -11,9 +11,12 @@ interface ITestReportPageProps {
 
 const TestReportPage: React.FC<ITestReportPageProps> = ({ organizations }) => {
   const { organizationId, fieldId } = useParams();
+  const [selectedTest, setSelectedTest] = useState<Test>();
+
   const field = organizations
     ?.find((o) => o.id === organizationId)
     ?.fields.find((f) => f.id === fieldId);
+  const tests = field?.tests;
 
   return (
     <div className={classes.main}>
@@ -22,7 +25,25 @@ const TestReportPage: React.FC<ITestReportPageProps> = ({ organizations }) => {
         <span className={classes.fieldTitle}>{field?.title}</span>
       </h1>
       <section>
-        
+        <form className={classes.inputs}>
+          <div>
+            <label>Select Test: </label>
+            <select
+              className={classes.select}
+              onChange={(e) =>
+                setSelectedTest(tests?.find((t) => t.id === e.target.value))
+              }
+            >
+              {tests?.map((test) => {
+                return (
+                  <option key={test.id} value={test.id}>
+                    {test.title}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </form>
       </section>
     </div>
   );
