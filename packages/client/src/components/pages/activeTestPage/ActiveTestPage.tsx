3b41c76e5f51714843ac135a1f0b4ef5f0a19878
Bar Spielberg <barspielberg@gmail.com>
2021-01-29 16:30:05 +0200

    student Login

--- packages/client/src/components/pages/activeTestPage/ActiveTestPage.tsx
+++ packages/client/src/components/pages/activeTestPage/ActiveTestPage.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { match } from "react-router";
+import StudentLogin from "./StudentLogin/StudentLogin";
 
 interface IActiveTestPageProps {
   match: match<{ testId: string }>;
 }
 //TODO by Bar
 const ActiveTestPage: React.FC<IActiveTestPageProps> = ({ match }) => {
-  return <div>ActiveTestPage Worked! id: {match.params.testId} </div>;
+  return (
+    <div>
+      <StudentLogin onStudentSubmited={(s) => console.log(s)} />
+      ActiveTestPage Worked! id: {match.params.testId}{" "}
+    </div>
+  );
 };
 
 export default ActiveTestPage;
