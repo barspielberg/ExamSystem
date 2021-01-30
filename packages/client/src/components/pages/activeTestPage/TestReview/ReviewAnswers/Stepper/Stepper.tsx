571eb97b536455d97c827917e547e7947a4ef3d5
Bar Spielberg <barspielberg@gmail.com>
2021-01-30 20:18:55 +0200

    stepper

--- packages/client/src/components/pages/activeTestPage/TestReview/ReviewAnswers/Stepper/Stepper.tsx
+++ packages/client/src/components/pages/activeTestPage/TestReview/ReviewAnswers/Stepper/Stepper.tsx
@@ -1,4 +1,6 @@
+import classes from "./Stepper.module.scss";
 import React from "react";
+import { Button } from "../../../../../uiElements";
 
 interface IStepperProps {
   current: number;
@@ -7,8 +9,34 @@ interface IStepperProps {
   moveTo: (index: number) => void;
 }
 
-const Stepper: React.FC<IStepperProps> = ({}) => {
-  return <div>Stepper Worked!</div>;
+const Stepper: React.FC<IStepperProps> = ({ current, max, moveTo, steps }) => {
+  return (
+    <div>
+      <div className={classes.btns}>
+        <Button disabled={current <= 0} onClick={() => moveTo(current - 1)}>
+          « Previous
+        </Button>
+        <Button disabled={current >= max} onClick={() => moveTo(current + 1)}>
+          Next »
+        </Button>
+      </div>
+      <div className={classes.steps}>
+        {steps.map((s) => (
+          <div
+            onClick={() => moveTo(s.index)}
+            key={s.index}
+            className={[
+              classes.step,
+              current === s.index ? classes.current : "",
+              s.ok ? classes.ok : "",
+            ].join(" ")}
+          >
+            {s.index + 1}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Stepper;
