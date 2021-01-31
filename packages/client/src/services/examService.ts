ad648007c8458b23b589fe376a9abfbfe89a2cbd
Michael <mishatolchinski@gmail.com>
2021-01-31 13:54:15 +0200

    tiny bug fix

--- packages/client/src/services/examService.ts
+++ packages/client/src/services/examService.ts
@@ -1,4 +1,3 @@
-
 import {
   Question,
   QuestionType,
@@ -58,44 +57,8 @@ class ExamService {
     }
   }
 
-  
+
 }
-
-export const calcGrade = (studentTest: TakenTest, questions: Question[]) => {
-  const numOfCurrect = getNumOfCorrectQuestions(studentTest, questions);
-  const grade = (numOfCurrect / questions.length) * 100;
-  return { grade, numOfCurrect };
-};
-
-const getNumOfCorrectQuestions = (
-  studentTest: TakenTest,
-  questions: Question[]
-) => {
-  return questions.reduce((pre, cur) => {
-    const answersId = studentTest.questions
-      .find((q) => q.oringinalQuestionId === cur.id)
-      ?.possibleAnswers.filter((a) => a.correct)
-      .map((a) => a.id);
-    if (!answersId) return pre;
-
-    const currectsId = cur.possibleAnswers
-      .filter((a) => a.correct)
-      .map((a) => a.id);
-    // console.log("answersId", answersId, "currectsId", currectsId);
-
-    if (cur.type === QuestionType.singleChoiceQuestion) {
-      return answersId.includes(currectsId[0]) ? pre + 1 : pre;
-    } else {
-      const currectOfAnswered =
-        answersId.filter((a) => currectsId.includes(a)).length /
-        answersId.length;
-      const answeredOfCurrect =
-        currectsId.filter((a) => answersId.includes(a)).length /
-        currectsId.length;
-      return pre + currectOfAnswered * answeredOfCurrect;
-    }
-  }, 0);
-};
 
 export default new ExamService();
 
