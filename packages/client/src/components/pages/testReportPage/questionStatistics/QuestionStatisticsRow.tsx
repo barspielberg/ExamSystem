711e0647088c549439a569932ff7daf6a894f126
Michael <mishatolchinski@gmail.com>
2021-02-01 17:54:33 +0200

    update questionStatistics

--- packages/client/src/components/pages/testReportPage/questionStatistics/QuestionStatisticsRow.tsx
+++ packages/client/src/components/pages/testReportPage/questionStatistics/QuestionStatisticsRow.tsx
@@ -1,5 +1,5 @@
 import { Question, TakenTest } from "@examsystem/common";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface IQuestionStatisticsRowProps {
   questions: Question[] | undefined;
@@ -10,28 +10,72 @@ const QuestionStatisticsRow: React.FC<IQuestionStatisticsRowProps> = ({
   questions,
   takenTests,
 }) => {
-  // const notEmpty = !!questions && questions.length > 0;
+  const [notEmpty, setNotEmpty] = useState(false);
+  const [questionStats, setQuestionStats] = useState<
+    {
+      id: string;
+      mainTitle: string;
+      tags: string[];
+      numofSub: number;
+      answeredCorrectly: number;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    setNotEmpty(!!questions && questions.length > 0);
+    const data = questions?.map<{
+      id: string;
+      mainTitle: string;
+      tags: string[];
+      numofSub: number;
+      answeredCorrectly: number;
+    }>((ques) => {
+      const numofSub = takenTests?.filter((tt) =>
+        tt.questions.filter((q) => q.oringinalQuestionId === ques.id)
+      )?.length;
+
+      const correct = takenTests?.filter((tt) =>
+        tt.questions.filter(
+          (q) =>
+            q.oringinalQuestionId === ques.id &&
+            q.possibleAnswers === ques.possibleAnswers
+        )
+      )?.length;
+
+      return {
+        id: ques.id,
+        mainTitle: ques.mainTitle,
+        tags: ques.tags,
+        numofSub: numofSub || 0,
+        answeredCorrectly: correct || 0,
+      };
+    });
+
+    console.log(data);
+  }, [questions, takenTests]);
 
   return (
     <React.Fragment>
-      {/* {notEmpty &&
+      {notEmpty &&
         questions?.map((question) => [
-          <tr>
+          <tr key={question.id}>
             <td>{question.id}</td>
-            <td>
-              {question.mainTitle}
+            <td style={{ textAlign: "revert" }}>
+              <div>
+                <h4>{question.mainTitle}</h4>
+                <p>
+                  {question.tags.map((t, index) => (
+                    <span key={index} style={{ color: "#3399ff" }}>
+                      {t} {index !== question.tags.length - 1 && "|"}
+                    </span>
+                  ))}
+                </p>
+              </div>
             </td>
-            <td>{#TODO get number of question submitions}</td>
-            <td>{#TODO Aswered correctly %}</td>
-          </tr>;
-        ])} */}
-      {/* test purpose only  */}
-      <tr>
-        <td>7</td>
-        <td>question text and tags</td>
-        <td>17</td>
-        <td>47%</td>
-      </tr>
+            <td>{0}</td>
+            <td>{0}</td>
+          </tr>,
+        ])}
     </React.Fragment>
   );
 };
