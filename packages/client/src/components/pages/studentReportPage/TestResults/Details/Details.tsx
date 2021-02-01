4c06e6ab1ad25dcedd307d54ad21f181c254d104
Bar Spielberg <barspielberg@gmail.com>
2021-02-01 16:45:56 +0200

    Expand All

--- packages/client/src/components/pages/studentReportPage/TestResults/Details/Details.tsx
+++ packages/client/src/components/pages/studentReportPage/TestResults/Details/Details.tsx
@@ -1,7 +1,7 @@
 import classes from "./Details.module.scss";
 import { Question, TakenTest } from "@examsystem/common";
 import React from "react";
-import { Table } from "../../../../uiElements";
+import { Button, Table } from "../../../../uiElements";
 import { useState } from "react";
 
 interface IDetailsProps {
@@ -10,9 +10,9 @@ interface IDetailsProps {
 }
 const titles = ["ID", "Qusetion", "Answered Correctly"];
 const Details: React.FC<IDetailsProps> = ({ answers, test }) => {
-  const [extendIds, setExtendIds] = useState<string[]>([]);
-  const extendHandler = (id: string) => {
-    setExtendIds((ids) => {
+  const [expandIds, setExpandIds] = useState<string[]>([]);
+  const expandHandler = (id: string) => {
+    setExpandIds((ids) => {
       if (ids.includes(id)) return ids.filter((i) => i !== id);
       return [...ids, id];
     });
@@ -24,15 +24,18 @@ const Details: React.FC<IDetailsProps> = ({ answers, test }) => {
         Click a question to see which of its answers were selected and whether
         they were correct, or click "Expand All" to see it for all questions.
       </p>
+      <Button onClick={() => setExpandIds(answers.map((a) => a.id))}>
+        Expand All
+      </Button>
       <Table titles={titles}>
         {answers.map((a) => [
-          <tr key={a.id} onClick={() => extendHandler(a.id)}>
+          <tr key={a.id} onClick={() => expandHandler(a.id)}>
             <td>{a.id}</td>
             <td>{a.mainTitle}</td>
-            <td>{a.id}</td>
+            <td>{getStatus(a, test)}</td>
           </tr>,
-          extendIds.includes(a.id) && (
-            <tr key={a.id + "522"} onClick={() => extendHandler(a.id)}>
+          expandIds.includes(a.id) && (
+            <tr key={a.id + "522"} onClick={() => expandHandler(a.id)}>
               <td colSpan={3} className={classes.full}>
                 <h5>Answers:</h5>
                 {a.possibleAnswers.map((p) => (
@@ -64,4 +67,21 @@ const getAnswerClasses = (right?: boolean, chosen?: boolean) => {
   if (!right && chosen) return classes.wrong;
   if (right) return classes.correct;
   return "";
+};
+const getStatus = (answer: Question, test: TakenTest) => {
+  const studentAnswer = test.questions.find(
+    (q) => q.oringinalQuestionId === answer.id
+  );
+
+  const studentSelections = studentAnswer?.possibleAnswers
+    .filter((p) => p.correct)
+    .map((p) => p.id);
+  const corrects = answer.possibleAnswers
+    .filter((p) => p.correct)
+    .map((p) => p.id);
+
+  return corrects.every((c) => studentSelections?.includes(c)) &&
+    studentSelections?.every((c) => corrects.includes(c))
+    ? "Correct"
+    : "False";
 };
